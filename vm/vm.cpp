e19db93c7fc31df55cdf4198227b6a587f764c06
Adam Herr <awherr@gmail.com>
2021-01-03 00:01:39 -0800

    Added simpile CLI interface to vm

--- vm/vm.cpp
+++ vm/vm.cpp
@@ -1,5 +1,16 @@
+#include <CLI/CLI.hpp>
 #include <iostream>
+#include <string>
 
-int main(int argc, char ** argv){
+struct VMAPP : CLI::App{
+    VMAPP():CLI::App("Mu Simulator"){
+        add_option("-o,--file", m_file_name, "Specify the file location of the input");
+    }
+    std::string m_file_name; 
+};
+
+int main(int argc, char **argv) {
+    VMAPP app;
+    CLI11_PARSE(app, argc, argv);
     return 0;
 }
